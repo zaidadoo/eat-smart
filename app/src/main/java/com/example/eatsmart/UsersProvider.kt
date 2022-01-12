0652a9a1f9ce2a22a6fa8949a9c6be868c59d353
Farah Ghanma <farahaghanma@gmail.com>
2022-01-12 12:40:55 +0200

    initial progress of Eat-Smart

--- app/src/main/java/com/example/eatsmart/UsersProvider.kt
+++ app/src/main/java/com/example/eatsmart/UsersProvider.kt
@@ -1,45 +1,53 @@
 package com.example.eatsmart
 
-import android.content.*
+import android.content.ContentProvider
+import android.content.ContentValues
 import android.database.Cursor
+import android.net.Uri
+import android.content.*
 import android.database.SQLException
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.database.sqlite.SQLiteQueryBuilder
-import android.net.Uri
+import android.provider.BaseColumns._ID
 import android.text.TextUtils
+import com.example.eatsmart.UsersProvider.Companion.CREATE_DB_TABLE
+import com.example.eatsmart.UsersProvider.Companion.USERS_TABLE_NAME
 import java.lang.IllegalArgumentException
 
-class StudentsProvider : ContentProvider() {
+class UsersProvider : ContentProvider() {
     companion object {
         val PROVIDER_NAME = "com.example.EatSmart.UsersProvider"
         val URL = "content://" + PROVIDER_NAME + "/users"
         val CONTENT_URI = Uri.parse(URL)
-        val _ID = "_id"
-        val NAME = "name"
-        val EMAIL = "email"
-        val PASSWORD = "password"
+
+        val userID = "userID"
+        val email = "email"
+        val fullName = "fullName"
+        val password = "password"
 
         val USERS = 1
         val USER_ID = 2
         val uriMatcher: UriMatcher? = null
-        val DATABASE_NAME = "ESUsers"
+        val DATABASE_NAME = "eatSmart"
         val USERS_TABLE_NAME = "users"
         val DATABASE_VERSION = 1
-        val CREATE_DB_TABLE = " CREATE TABLE " + USERS_TABLE_NAME +
-                " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " + " name TEXT NOT NULL, " +
-                " email TEXT NOT NULL " +
-                " password TEXT NOT NULL);"
-        private var sUriMatcher = UriMatcher(UriMatcher.NO_MATCH);
-        init
-        {
-            sUriMatcher.addURI(PROVIDER_NAME, "users", USERS);
-            sUriMatcher.addURI(PROVIDER_NAME, "users/#", USER_ID);
-        }
 
+        val CREATE_DB_TABLE = "CREATE TABLE users (userID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "fullName TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL)"
     }
-    private var db: SQLiteDatabase? = null
 
+    private var sUriMatcher = UriMatcher(UriMatcher.NO_MATCH);
+    init
+    {
+        sUriMatcher.addURI(PROVIDER_NAME, "users", USERS);
+        sUriMatcher.addURI(PROVIDER_NAME, "users/#", USER_ID);
+    }
+
+    private var db: SQLiteDatabase? = null
+    /**
+     * Helper class that actually creates and manages * the provider's underlying data repository.
+     */
     private class DatabaseHelper internal constructor(context: Context?) :
         SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
         override fun onCreate(db: SQLiteDatabase) {
@@ -58,18 +66,49 @@ class StudentsProvider : ContentProvider() {
         /**
          * Create a write able database which will trigger its
          * creation if it doesn't already exist.  */
+
         db = dbHelper.writableDatabase
         return if (db == null) false else true
     }
 
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?
+    ): Cursor? {
+
+        var sortOrder = sortOrder
+        val qb = SQLiteQueryBuilder()
+        qb.tables = USERS_TABLE_NAME
+        when (uriMatcher!!.match(uri)) {
+            USER_ID -> qb.appendWhere(userID + "=" + uri.pathSegments[1])
+            else -> {
+                null
+            }
+        }
+
+        if (sortOrder == null || sortOrder === "") {
+            sortOrder = fullName
+        }
+
+        val c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder)
+        c.setNotificationUri(context!!.contentResolver, uri)
+        return c
+    }
+
+    override fun getType(uri: Uri): String? {
+        when (uriMatcher!!.match(uri)) {
+            USERS -> return "vnd.android.cursor.dir/vnd.example.users"
+            USER_ID -> return "vnd.android.cursor.item/vnd.example.users"
+            else -> throw IllegalArgumentException("Unsupported URI: $uri")
+        }
+    }
+
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        /**
-         * Add a new student record
-         */
+        //values are sent in SignUp activity (ContentValues variable called "values")
         val rowID = db!!.insert(USERS_TABLE_NAME, "", values)
-        /**
-         * If record is added successfully
-         */
         if (rowID > 0) {
             val _uri = ContentUris.withAppendedId(CONTENT_URI, rowID)
             context!!.contentResolver.notifyChange(_uri, null)
@@ -78,52 +117,23 @@ class StudentsProvider : ContentProvider() {
         throw SQLException("Failed to add a record into $uri")
     }
 
-    override fun query(
-        uri: Uri, projection: Array<String>?,
-        selection: String?, selectionArgs: Array<String>?, sortOrder: String?
-    ): Cursor? {
-        var sortOrder = sortOrder
-        val qb = SQLiteQueryBuilder()
-        qb.tables = USERS_TABLE_NAME
-        if (uriMatcher != null) {
-            when (uriMatcher.match(uri)) {
-                USER_ID -> qb.appendWhere(_ID + "=" + uri.pathSegments[1])
-                else -> {null
-                }
-            }
-        }
-
-        if (sortOrder == null || sortOrder === "") {
-            /**
-             * By default sort on student names
-             */
-            sortOrder = NAME
-        }
-        val c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder)
-        /**
-         * register to watch a content URI for changes  */
-        c.setNotificationUri(context!!.contentResolver, uri)
-        return c
-    }
-
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        var count = 0
-        when (uriMatcher!!.match(uri)) {
+        var count = 0;
+        when(uriMatcher!!.match(uri)){
             USERS -> count = db!!.delete(
-                USERS_TABLE_NAME, selection,
-                selectionArgs
+                USERS_TABLE_NAME, selection, selectionArgs
             )
+
             USER_ID -> {
                 val id = uri.pathSegments[1]
                 count = db!!.delete(
                     USERS_TABLE_NAME,
-                    _ID + " = " + id +
-                            if (!TextUtils.isEmpty(selection)) " AND ($selection)" else "",
-                    selectionArgs
+                    _ID + "=" + id + if(!TextUtils.isEmpty(selection)) " AND ($selection)" else "", selectionArgs
                 )
             }
             else -> throw IllegalArgumentException("Unknown URI $uri")
         }
+
         context!!.contentResolver.notifyChange(uri, null)
         return count
     }
@@ -152,11 +162,4 @@ class StudentsProvider : ContentProvider() {
         return count
     }
 
-    override fun getType(uri: Uri): String? {
-        when (uriMatcher!!.match(uri)) {
-            USERS -> return "vnd.android.cursor.dir/vnd.example.users"
-            USER_ID -> return "vnd.android.cursor.item/vnd.example.users"
-            else -> throw IllegalArgumentException("Unsupported URI: $uri")
-        }
-    }
 }
