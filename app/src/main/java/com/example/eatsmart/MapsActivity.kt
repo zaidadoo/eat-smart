6d1d95e2e655e2e3f3791b6025f031a6abcf025f
Farah Ghanma <farahaghanma@gmail.com>
2022-01-13 18:21:05 +0200

    finalized map locations according to selected allergies/diseases

--- app/src/main/java/com/example/eatsmart/MapsActivity.kt
+++ app/src/main/java/com/example/eatsmart/MapsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.eatsmart
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -16,6 +19,10 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.eatsmart.databinding.ActivityMapsBinding
 import com.google.android.gms.location.LocationServices
+import java.util.ArrayList
+
+
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -35,31 +42,94 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val r1 = LatLng(31.973188,35.841687)
-        val r2 = LatLng(31.973312,35.841313)
-        val r3 = LatLng(31.974312,35.846563)
-        val r4 = LatLng(31.973687,35.840312)
-        val r5 = LatLng(31.974562,35.848813)
-        val r6 = LatLng(31.975062,35.852812)
-        mMap.addMarker(MarkerOptions().position(r1).title("Ray's Fried Chicken"))
-        mMap.addMarker(MarkerOptions().position(r2).title("Avotaco"))
-        mMap.addMarker(MarkerOptions().position(r3).title("The Fries Shop"))
-        mMap.addMarker(MarkerOptions().position(r4).title("Ahwitna Cafe"))
-        mMap.addMarker(MarkerOptions().position(r5).title("DAJAJ"))
-        mMap.addMarker(MarkerOptions().position(r6).title("Shawerma Al Hakam"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
+        val allergiesList = intent.getSerializableExtra("key") as ArrayList<String>
+
+        Toast.makeText(applicationContext, "The allergies/diseases selected: " +
+                allergiesList.toString(), Toast.LENGTH_SHORT).show()
+
+        if(allergiesList.contains("celiac")){
+            val r1 = LatLng(31.952562,35.855812)
+            mMap.addMarker(MarkerOptions().position(r1).title("Cozmo"))
+
+            val r2 = LatLng(31.949312,35.929063)
+            mMap.addMarker(MarkerOptions().position(r2).title("Al-Sufara"))
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
+        }
+
+        if(allergiesList.contains("hypertension")){
+            val r1 = LatLng(31.956688,35.859562)
+            mMap.addMarker(MarkerOptions().position(r1).title("TheFitFactory"))
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
+        }
+
+        if(allergiesList.contains("marasmus")){
+            val r1 = LatLng(31.952562,35.855812)
+            mMap.addMarker(MarkerOptions().position(r1).title("Cozmo"))
+
+            val r2 = LatLng(31.975563,35.884313)
+            mMap.addMarker(MarkerOptions().position(r2).title("Healthy Fresh Amman"))
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
+        }
+
+        if(allergiesList.contains("diabetes")){
+            val r1 = LatLng(31.957813,35.865938)
+            mMap.addMarker(MarkerOptions().position(r1).title("OJ's - Super Fast Salads"))
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
+        }
+
+        if(allergiesList.contains("anemia")){
+            val r1 = LatLng(31.939813,35.855937)
+            mMap.addMarker(MarkerOptions().position(r1).title("Salad Mania"))
+
+            val r2 = LatLng(31.953313,35.884062)
+            mMap.addMarker(MarkerOptions().position(r2).title("Seed"))
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
+        }
+
+        if(allergiesList.contains("rickets")){
+            val r1 = LatLng(31.982188,35.875812)
+            mMap.addMarker(MarkerOptions().position(r1).title("Muscle Kitchen"))
+
+            val r2 = LatLng(31.948313,35.865813)
+            mMap.addMarker(MarkerOptions().position(r2).title("Primrose Cafe"))
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
+        }
+
+        if(allergiesList.contains("kwashiorkor")){
+            val r1 = LatLng(31.979312,35.856188)
+            mMap.addMarker(MarkerOptions().position(r1).title("Fitness Kitchen"))
+
+            val r2 = LatLng(31.981187,35.878438)
+            mMap.addMarker(MarkerOptions().position(r2).title("Juicitarian"))
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
+        }
+
+        //these were all test values
+//        val r1 = LatLng(31.973188,35.841687)
+//        val r2 = LatLng(31.973312,35.841313)
+//        val r3 = LatLng(31.974312,35.846563)
+//        val r4 = LatLng(31.973687,35.840312)
+//        val r5 = LatLng(31.974562,35.848813)
+//        val r6 = LatLng(31.975062,35.852812)
+//        mMap.addMarker(MarkerOptions().position(r1).title("Ray's Fried Chicken"))
+//        mMap.addMarker(MarkerOptions().position(r2).title("Avotaco"))
+//        mMap.addMarker(MarkerOptions().position(r3).title("The Fries Shop"))
+//        mMap.addMarker(MarkerOptions().position(r4).title("Ahwitna Cafe"))
+//        mMap.addMarker(MarkerOptions().position(r5).title("DAJAJ"))
+//        mMap.addMarker(MarkerOptions().position(r6).title("Shawerma Al Hakam"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(r1))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-
-        val allergiesList = intent.getSerializableExtra( "key" )
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -73,14 +143,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location : Location? ->
                 if (location != null) {
-                    Toast.makeText(this, location.latitude.toString(), Toast.LENGTH_LONG).show()
+                    //Toast.makeText(this, location.latitude.toString(), Toast.LENGTH_LONG).show()
                 }
                 // Got last known location. In some rare situations this can be null.
             }
 //        binding = ActivityMapsBinding.inflate(layoutInflater)
 //        setContentView(binding.root)
 
-
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun onRequestPermissionsResult(
@@ -93,5 +166,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         {
             Toast.makeText(this, "Could not get location", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.appServices -> {
+                val intent = Intent(this, AppServices::class.java)
+                startActivity(intent)
+            }
+
+            R.id.userSettings -> {
+                val intent = Intent(this, UserSettings::class.java)
+                startActivity(intent)
+            }
+
+            R.id.editAllergiesDiseases -> {
+                val intent = Intent(this, AllergiesSelection::class.java)
+                startActivity(intent)
+            }
+
+            R.id.signOut -> {
+                val intent = Intent(this, SignIn::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
